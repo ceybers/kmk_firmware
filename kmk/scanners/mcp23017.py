11014174cd20407c411a058bda103e6ac5682496
Craig Eybers <craig.eybers@gmail.com>
2023-02-12 19:32:14 +0200

    Refactored list comprehension

--- kmk/scanners/mcp23017.py
+++ kmk/scanners/mcp23017.py
@@ -1,59 +1,48 @@
 import digitalio
-# https://github.com/adafruit/Adafruit_CircuitPython_MCP230xx
 from adafruit_mcp230xx.mcp23017 import MCP23017
 from keypad import Event as KeyEvent
 from kmk.scanners.keypad import KeypadScanner
 
-def intify_coordinate(row, col, len_cols):
-    return len_cols * row + col
-
 class MCPScanner(KeypadScanner):
-    def __init__(self, row_pins, col_pins, i2c, i2c_address, offset):
+    def __init__(self, row_pin_numbers, col_pin_numbers, i2c, i2c_address, offset):
         mcp = MCP23017(i2c, address=i2c_address)
 
-        self.row_pins = []
-        for pin_number in row_pins:
-            pin = mcp.get_pin(pin_number)
-            self.row_pins.append(pin)
-            pin.switch_to_output(value=True)
-            pin.value = False
-        
-        self.col_pins = []
-        for pin_number in col_pins:
-            pin = mcp.get_pin(pin_number)
-            self.col_pins.append(pin)
-            pin.direction = digitalio.Direction.INPUT
-            pin.pull = digitalio.Pull.UP
+        self.row_pins = [mcp.get_pin(n) for n in row_pin_numbers]
+        [p.switch_to_output(value=False) for p in self.row_pins]
+        self.row_count = len(self.row_pins)
 
-        self.previousState = []
-        for r in range(len(self.row_pins)):
-            new = []
-            for c in range(len(self.col_pins)):
-                new.append(True)
-            self.previousState.append(new)
+        self.col_pins = [mcp.get_pin(n) for n in col_pin_numbers]
+        [p.switch_to_input(pull=digitalio.Pull.UP) for p in self.col_pins]
+        self.col_count = len(self.col_pins)
+
+        self.previous_state = [[True for c in self.col_pins] for r in self.row_pins]
+        
         self.offset = offset
         self.events = []
-
+        
         super().__init__()
 
     @property
     def key_count(self):
         return (len(self.row_pins) * len(self.col_pins))
 
+    def intify_coordinate(self, row, col):
+        return (self.col_count * row + col) + self.offset
+
     def scan_for_changes(self):
         if self.events:
-            return self.events.pop()
+            return self.events.pop(0)
+        
+        for r in self.row_pins:
+            r.value = True
 
-        for r in range(len(self.row_pins)):
-            self.row_pins[r].value = True
-
-        for r in range(len(self.row_pins)):
-            self.row_pins[r].value = False
-            for c in range(len(self.col_pins)):
-                currentState = self.col_pins[c].value
-                if self.col_pins[c].value != self.previousState[r][c]:
-                    self.previousState[r][c] = currentState
-                    ev = KeyEvent(intify_coordinate(r, c, len(self.col_pins)) + self.offset, not currentState)
+        for row_number, row_pin in enumerate(self.row_pins):
+            row_pin.value = False
+            for col_number, col_pin in enumerate(self.col_pins):
+                current_state = col_pin.value
+                if current_state != self.previous_state[row_number][col_number]:
+                    self.previous_state[row_number][col_number] = current_state
+                    ev = KeyEvent(self.intify_coordinate(row_number, col_number), not current_state)
                     self.events.append(ev)
-            self.row_pins[r].value = True
+            row_pin.value = True
         return None
