29beef669acf5aec66735a61569f5d64d5941dba
Craig Eybers <craig.eybers@gmail.com>
2023-02-17 15:52:01 +0200

    Updated validator class and docs

--- docs/slowmods.md
+++ docs/slowmods.md
@@ -1,10 +1,35 @@
 # Slow Modifiers
 
-This module provides a set of modifier keycodes designed to work around an issue with sending modified keycodes over Remote Desktop or Hyper-V.
+This module provides a workaround to address issues with sending modified keycodes on Windows over Hyper-V, Remote Desktop, or Spice.
 
-It will automatically apply to [US ANSI Shifted Symbols](keycodes.md#us-ansi-shifted-symbols) keycodes, i.e. shifted keycodes between `KC.TILD` and `KC.QUES`. 
+Additionally, tt will automatically apply to [US ANSI Shifted Symbols](keycodes.md#us-ansi-shifted-symbols) keycodes, i.e. shifted keycodes between `KC.TILD` and `KC.QUES`. 
+
+## Example
+```python
+from kmk.modules.slowmods import SlowMods
+
+keyboard.modules.append(SlowMods())
+
+SEL_ALL = KC.SL(KC.A, KC.LCTL)
+
+keyboard.keymap = [
+	[
+        SEL_ALL,
+    ],
+]
+```
+
+## Keycodes
+|Keycode                         | Description              |
+|--------------------------------|--------------------------|
+|SEL_ALL = KC.LT(base, modifier) | mod + base               |
+|SEL_ALL = KC.LT(KC.A, KC.LCTL)  | `LCTL` + `A`             |
+|SLOW_AT = KC.LT(KC.N2,KC.LSFT)  | `LSFT` + `2`             |
+|S_ALT_F = KC.LT(KC.F, KC.LALT)  | `LALT` + `F`             |
+|S_GUI_E = KC.LT(KC.E, KC.LGUI)  | `LGUI` + `E`             |
+
+
 ## Issue
-
 Sending a key such as `KC.LCTL(C)` via these clients occasionally results in the following behaviour:
 
 ```
@@ -22,32 +47,4 @@ KEY-UP   - QMK: KC_C    Event key: c           Code: KeyC          KeyCode: 67 i
 KEY-UP   - QMK: KC_LCTL Event key: Control     Code: ControlLeft   KeyCode: 17 in 158.335ms
 ```
 
-This module works around this issue by only sending the `KEY-UP` for the modifier (`KC.LCTL` in this case) during the `on_release` event rather than during the `on_press` event.
-
-## Example
-```python
-from kmk.modules.slowmods import SlowMods
-
-keyboard.modules.append(SlowMods())
-
-keyboard.keymap = [
-	[
-        KC.SCTL(KC.A),   # Ctrl+A
-    ],
-]
-```
-
-## Keycodes
-
-|Keycode                   | Description                   |
-|--------------------------|-------------------------------|
-|SEL_ALL = KC.SCTL(KC.A)   | `LCTL` + `A`                  |
-|SLOW_AT = KC.SSFT(KC.N2)  | `LSFT` + `2`                  |
-|S_ALT_F = KC.SALT(KC.F)   | `LALT` + `F`                  |
-|S_GUI_E = KC.SGUI(KC.E)   | `LGUI` + `E`                  |
-|TEST    = KC.SCS(KC.RGHT) | `LCTL` + `LSFT` + right arrow |
-
-## Compatibility Issues
-
- - ✅ Works with [Combos/Chords](combos.md), both as the trigger and the output.
- - ❌ Doesn't work with [ModTap](modtap.md) (i.e. `KC.MT()`), neither as tap nor as hold.
+This module works around this issue by first sending the modifier key press as a separate HID event, then sending the base key as a separate key press. This is as opposed to sending on HID event with the modifier applied to the base key.
--- kmk/modules/slowmods.py
+++ kmk/modules/slowmods.py
@@ -1,96 +1,36 @@
-'''Modifier keys designed to work over Remote Desktop/Hyper-V'''
 from kmk.keys import KC, make_argumented_key
 from kmk.modules import Module
-from kmk.types import HoldTapKeyMeta
 
-def slowmod_validator(kc, tap_time=None):
-    return HoldTapKeyMeta(kc=kc, prefer_hold=False, tap_time=tap_time)
+
+class SlowModMeta:
+    def __init__(self, kc, mod):
+        self.kc = kc
+        self.mod = mod
+
 
 class SlowMods(Module):
     def __init__(self):
         self._shifted_numbers = range(KC.EXCLAIM.code, KC.QUESTION.code)
+
         make_argumented_key(
-            validator=slowmod_validator,
-            names=('SCTL',),
-            on_press=self.slowmod_pressed,
-            on_release=self.slowmod_released, 
-            has_modifiers={KC.LCTL.code}
-        )
-        make_argumented_key(
-            validator=slowmod_validator,
-            names=('SSFT',),
-            on_press=self.slowmod_pressed,
-            on_release=self.slowmod_released, 
-            has_modifiers={KC.LSFT.code}
-        )
-        make_argumented_key(
-            validator=slowmod_validator,
-            names=('SALT',),
-            on_press=self.slowmod_pressed,
-            on_release=self.slowmod_released, 
-            has_modifiers={KC.LALT.code}
-        )
-        make_argumented_key(
-            validator=slowmod_validator,
-            names=('SGUI',),
-            on_press=self.slowmod_pressed,
-            on_release=self.slowmod_released, 
-            has_modifiers={KC.LGUI.code}
-        )
-        make_argumented_key(
-            validator=slowmod_validator,
-            names=('SCS',),
-            on_press=self.slowmod_pressed,
-            on_release=self.slowmod_released, 
-            has_modifiers={KC.LCTL.code, KC.LSFT.code, }
+            names=('SL',),
+            validator=SlowModMeta,
+            on_press=self.sl_pressed,
+            on_release=self.sl_released,
         )
 
-    def code_to_key(self, code):
-        mod_key = KC.NO 
-        if code == KC.LCTL.code:
-            mod_key = KC.LCTL
-        elif code == KC.LSFT.code:
-            mod_key = KC.LSFT
-        elif code == KC.LALT.code:
-            mod_key = KC.LALT
-        elif code == KC.LGUI.code:
-            mod_key = KC.LGUI
-        else:
-            raise ValueError('SlowMods code_to_key() tried to convert an invalid code to a key')
-        return mod_key
-
-    def slowmod_pressed(self, key, keyboard, *args, **kwargs):
-        for mod in key.has_modifiers:
-            keyboard.process_key(self.code_to_key(mod), True)
-        keyboard._send_hid()
-        keyboard.process_key(key.meta.kc, True)
-        return keyboard
-
-    def slowmod_released(self, key, keyboard, *args, **kwargs):
-        keyboard.process_key(key.meta.kc, False)
-        keyboard._send_hid()
-        for mod in key.has_modifiers:
-            keyboard.process_key(self.code_to_key(mod), False)
-        return keyboard
-    
-    def process_key(self, keyboard, key, is_pressed, int_coord):
-        if key.has_modifiers is not None:
-            if KC.LSFT.code in key.has_modifiers:
-                if key.code in self._shifted_numbers:
-                    keyboard.process_key(KC.LSFT, is_pressed)
-                    keyboard._send_hid()
-        return key
-
-    # Excluding this results in the error:
-    # Failed to load module  <SlowMods object at 0x20009a50>
     def during_bootup(self, keyboard):
         return
 
     def before_matrix_scan(self, keyboard):
         return
 
-    def after_matrix_scan(self, keyboard):
-        return
+    def process_key(self, keyboard, key, is_pressed, int_coord):
+        if key.code in self._shifted_numbers:
+            if KC.LSFT.code in key.has_modifiers:
+                keyboard.process_key(KC.LSFT, is_pressed)
+                keyboard._send_hid()
+        return key
 
     def before_hid_send(self, keyboard):
         return
@@ -98,11 +38,28 @@ class SlowMods(Module):
     def after_hid_send(self, keyboard):
         return
 
-    #def matrix_detected_press(self, keyboard):
-    #    return
+    def on_powersave_enable(self, keyboard):
+        return
 
-    #def on_powersave_enable(self, keyboard):
-    #    return
+    def on_powersave_disable(self, keyboard):
+        return
 
-    #def on_powersave_disable(self, keyboard):
-    #    return
+    def after_matrix_scan(self, keyboard):
+        return
+
+    def release_key(self, keyboard, key):
+        return
+
+    def sl_pressed(self, key, keyboard, *args, **kwargs):
+        keyboard.process_key(key.meta.mod, True)
+        keyboard._send_hid()
+        keyboard.process_key(key.meta.kc, True)
+        keyboard._send_hid()
+        return
+
+    def sl_released(self, key, keyboard, *args, **kwargs):
+        keyboard.process_key(key.meta.kc, False)
+        keyboard._send_hid()
+        keyboard.process_key(key.meta.mod, False)
+        keyboard._send_hid()
+        return
